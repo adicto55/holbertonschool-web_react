b915851484d336e51141df77f9abc83c841fa1e0
Your Name <you@example.com>
2026-08-12 14:50:43 +0400

    sd

--- 0x02-react_intro/task_0/dashboard/src/App.jsx
+++ 0x02-react_intro/task_0/dashboard/src/App.jsx
@@ -9,15 +9,16 @@ function App() {
         <img src={holberton_logo} />
         <h1>School dashboard</h1>
       </div>
+
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
-      </div>
+
+      <footer className="App-footer">
+        <p>copyright - Holberton School</p>
+      </footer>
     </>
   );
 }
 
 export default App;
-
