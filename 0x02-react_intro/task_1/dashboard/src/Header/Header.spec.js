0305e8639bbe850bab6d3977b8e6998cab9af43c
Your Name <you@example.com>
2026-08-19 19:16:03 +0400

    Sd

--- 0x02-react_intro/task_1/dashboard/src/Header/Header.spec.js
+++ 0x02-react_intro/task_1/dashboard/src/Header/Header.spec.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header component', () => {
   test('renders the Holberton logo', () => {
-    const { container } = render(<Header />);
+    render(<Header />);
 
-    const image = container.querySelector('img');
-    expect(image).toBeInTheDocument();
-    expect(image.alt).toBe('holberton logo');
+    expect(screen.getByAltText('holberton logo')).toBeInTheDocument();
   });
 
-  test('renders the correct heading', () => {
-    const { container } = render(<Header />);
+  test('renders the heading with the correct text', () => {
+    render(<Header />);
 
-    const heading = container.querySelector('h1');
-    expect(heading).toBeInTheDocument();
-    expect(heading.textContent).toBe('School dashboard');
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'School dashboard'
+    );
   });
 });
